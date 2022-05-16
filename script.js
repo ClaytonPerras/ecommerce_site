9ca8a4c98730ad33556e75a9556704e6ac837f5a
ClaytonPerras <claytonperras@gmail.com>
2022-05-16 14:10:16 -0400

    Moved off Bootstrap. Added product section.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const button = document.querySelectorAll('.product-button');
+console.log(button);
+
+button.forEach(addToCart);
+
+function addToCart(button) {
+  button.addEventListener('click', (event) => {
+    button.style.backgroundColor = '#a5b4a4';
+  });
+}
